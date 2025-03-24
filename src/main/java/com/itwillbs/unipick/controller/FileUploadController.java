4f96720e769e51803a6f397ed1bd69c28d911f99
Junesse <junesse89@gmail.com>
2025-03-24 20:02:25 +0900

    상세설명 이미지 되네?;;;

--- src/main/java/com/itwillbs/unipick/controller/FileUploadController.java
+++ src/main/java/com/itwillbs/unipick/controller/FileUploadController.java
@@ -18,8 +18,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class FileUploadController {
 	@PostMapping("/upload")
-    public ResponseEntity<Map<String, String>> handleFileUpload(@RequestParam("image") MultipartFile file) {
-		System.out.println("!~@#$%^&*"+file);
+    public ResponseEntity<Map<String, String>> handleFileUpload(@RequestParam("file") MultipartFile file) {
+		 System.out.println("🚀 파일 업로드 시도: " + file.getOriginalFilename());
         try {
             String uploadDirectory = "D:/UNIPICK/src/main/webapp/resources/upload/";
 
